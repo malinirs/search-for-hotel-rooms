d8d37bdba73ff6930b127bd42a6ec1778e9c3e93
Kris <krisriva86@gmail.com>
2024-06-13 17:20:48 +0300

    Pug is converted to HTML

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './styles/style.css';
-import './styles/scss.scss';
-
-console.log('Util Id:');
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HTMLWebpackPugPlugin = require('html-webpack-pug-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -18,8 +17,8 @@ const cssLoaders = extra => {
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  // entry: './index.pug',
-  entry: './index.js',
+  entry: {},
+  // entry: './index.js',
   output: {
     filename : filename('js'),
     path: path.resolve(__dirname, 'dist')
@@ -30,13 +29,9 @@ module.exports = {
     hot: isDev
   },
   plugins: [
-    new HTMLWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-      filename: './index.html'
-    }),
+    new HTMLWebpackPlugin({ template: './index.pug' }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({ filename: filename('css') }),
-    new HTMLWebpackPugPlugin({ template: './src/index.pug' }),
   ],
   module: {
     rules: [
@@ -47,6 +42,64 @@ module.exports = {
     ]
   }
 }
+
+
+
+
+// const path = require('path');
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HTMLWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+
+// var template = require("./file.pug");
+// var html = require("apply-loader!pug-loader!./file.pug");
+
+
+// const isDev = process.env.NODE_ENV === 'development';
+// const isProd = !isDev;
+// const filename = ext => isDev ? `[name].${ext}` : `[name].[fullhash].${ext}`;
+
+// const cssLoaders = extra => {
+//   const loaders = [MiniCssExtractPlugin.loader, 'css-loader']
+//   if (extra) {
+//     loaders.push(extra)
+//   }
+//   return loaders
+// }
+
+// module.exports = {
+//   context: path.resolve(__dirname, 'src'),
+//   // entry: './index.pug',
+//   entry: './index.js',
+//   output: {
+//     filename : filename('js'),
+//     path: path.resolve(__dirname, 'dist')
+//   },
+//   devServer: {
+//     static: { directory: './src', },
+//     port: 4200,
+//     hot: isDev
+//   },
+//   plugins: [
+//     new HTMLWebpackPlugin({
+//       template: path.join(__dirname, 'src', 'index.html'),
+//       filename: './index.html'
+//     }),
+//     new CleanWebpackPlugin(),
+//     new MiniCssExtractPlugin({ filename: filename('css') }),
+//     new HTMLWebpackPugPlugin({ template: './src/index.pug' }),
+//   ],
+//   module: {
+//     rules: [
+//       { test: /\.css$/, use: cssLoaders() },
+//       { test: /\.(png|jpg|svg|gif)$/, type: 'asset/resource' },
+//       { test: /\.pug$/, loader: 'pug-loader', },
+//       { test: /\.s[ac]ss$/, use: cssLoaders('sass-loader') },
+//     ]
+//   }
+// }
 
 
 
@@ -71,57 +124,6 @@ module.exports = {
 // module.exports = {
 //   context: path.resolve(__dirname, 'src'),
 //   // entry: './index.pug',
-//   entry: {},
-//   output: {
-//     filename : filename('html'),
-//     path: path.resolve(__dirname, 'dist')
-//   },
-//   devServer: {
-//     static: { directory: './src', },
-//     port: 4200,
-//     hot: isDev
-//   },
-//   plugins: [
-//     new HTMLWebpackPlugin({
-//       template: path.join(__dirname, 'src', 'index.pug'),
-//       filename: './index.html'
-//     }),
-//     new CleanWebpackPlugin(),
-//     new MiniCssExtractPlugin({ filename: filename('css') }),
-//     new HTMLWebpackPugPlugin({ template: './src/index.pug' }),
-//   ],
-//   module: {
-//     rules: [
-//       { test: /\.css$/, use: cssLoaders() },
-//       { test: /\.(png|jpg|svg|gif)$/, type: 'asset/resource' },
-//       { test: /\.pug$/, loader: 'pug-loader', },
-//       { test: /\.s[ac]ss$/, use: cssLoaders('sass-loader') },
-//     ]
-//   }
-// }
-
-
-
-
-// const path = require('path');
-// const HTMLWebpackPlugin = require('html-webpack-plugin');
-// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-// const isDev = process.env.NODE_ENV === 'development';
-// const isProd = !isDev;
-// const filename = ext => isDev ? `[name].${ext}` : `[name].[fullhash].${ext}`;
-
-// const cssLoaders = extra => {
-//   const loaders = [MiniCssExtractPlugin.loader, 'css-loader']
-//   if (extra) {
-//     loaders.push(extra)
-//   }
-//   return loaders
-// }
-
-// module.exports = {
-//   context: path.resolve(__dirname, 'src'),
 //   entry: './index.js',
 //   output: {
 //     filename : filename('js'),
@@ -134,17 +136,19 @@ module.exports = {
 //   },
 //   plugins: [
 //     new HTMLWebpackPlugin({
-//       template: path.join(__dirname, 'src', 'index.pug'),
+//       template: path.join(__dirname, 'src', 'index.html'),
 //       filename: './index.html'
 //     }),
 //     new CleanWebpackPlugin(),
-//     new MiniCssExtractPlugin({ filename: filename('css') })
+//     new MiniCssExtractPlugin({ filename: filename('css') }),
+//     new HTMLWebpackPugPlugin({ template: './src/index.pug' }),
 //   ],
 //   module: {
 //     rules: [
 //       { test: /\.css$/, use: cssLoaders() },
 //       { test: /\.(png|jpg|svg|gif)$/, type: 'asset/resource' },
 //       { test: /\.pug$/, loader: 'pug-loader', },
+//       { test: /\.s[ac]ss$/, use: cssLoaders('sass-loader') },
 //     ]
 //   }
 // }
