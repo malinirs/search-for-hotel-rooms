8b293a0e874be70e29de9293b3791603de44a2fe
Kris <krisriva86@gmail.com>
2025-08-28 16:47:49 +0300

    implementation of buttons and counters in dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,25 +1,10 @@
-// function initDropdown() {
-//   const dropdowns = document.querySelectorAll('.dropdown');
-//   const insidesDropdowns = document.querySelectorAll('.dropdown__item, .dropdown__button');
-  
-//   dropdowns.forEach(dropdown => {
-//     dropdown.addEventListener('click', () => {
-//       dropdown.classList.toggle('dropdown--expanded');
-//     });
-
-//     insidesDropdowns.forEach(item => {
-//       item.addEventListener('click', event => {
-//         event.stopPropagation();
-//       })
-//     })
-//   });
-// };
-
 function initDropdown() {
   const dropdowns = document.querySelectorAll('.dropdown');
 
   dropdowns.forEach(dropdown => {
     const header = dropdown.querySelector('.dropdown__header');
+    const title = dropdown.querySelector('.dropdown__title');
+    const initialTitle = title.textContent;
     const plusButtons = dropdown.querySelectorAll('.dropdown__button--plus');
     const minusButtons = dropdown.querySelectorAll('.dropdown__button--minus');
     const clearButton = dropdown.querySelector('.dropdown__footer--clear');
@@ -29,15 +14,53 @@ function initDropdown() {
       dropdown.classList.toggle('dropdown--expanded');
     });
 
+    function increaseValue(button) {
+      const elementCounter = button.previousElementSibling;
+      elementCounter.textContent = (parseInt(elementCounter.textContent, 10) + 1).toString();
+    }
+
+    function decreaseValue(button) {
+      const elementCounter = button.nextElementSibling;
+      const valueCounter = parseInt(elementCounter.textContent, 10);
+      if (valueCounter > 0) {
+        elementCounter.textContent = (valueCounter - 1).toString();
+      }
+    }
+
+    function clearValue() {
+      const counters = dropdown.querySelectorAll('.dropdown__value');
+      counters.forEach(counter => counter.textContent = '0');
+      title.textContent = initialTitle;
+    }
+
     [...plusButtons, ...minusButtons, clearButton].forEach(button => {
-      button.addEventListener('click', (event) => {
-        event.stopPropagation();
-      })
-    })
+        button.addEventListener('click', (event) => {
+          event.stopPropagation();
+          if (button.classList.contains('dropdown__button--plus')) {
+            increaseValue(button);
+          } else if (button.classList.contains('dropdown__button--minus')) {
+            decreaseValue(button);
+          } else if (button.classList.contains('dropdown__footer--clear')) {
+            clearValue();
+          }
+        })
+      });
 
     applyButton.addEventListener('click', (event) => {
       event.stopPropagation();
-      dropdown.classList.remove('dropdown--expanded');
+      const counters = Array.from(dropdown.querySelectorAll('.dropdown__value'));
+      const sum = counters.reduce((currentSum, currentNumber) => {
+        return currentSum + parseInt(currentNumber.textContent, 10);
+      }, 0);
+      if (sum === 1) {
+        title.textContent = sum.toString() + ' гость';
+      } else if (sum > 1 && sum < 5) {
+        title.textContent = sum.toString() + ' гостя';
+      } else if (sum > 4) {
+        title.textContent = sum.toString() + ' гостей';
+      } else {
+        title.textContent = initialTitle;
+      }
     });
   })
 };
