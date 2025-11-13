b3cb1585a8ac368eb8bfe5411776f8ce1cc257d2
Kris <krisriva86@gmail.com>
2025-11-13 14:14:40 +0300

    improved js logic in the Room Convenience dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -20,17 +20,58 @@ function initDropdown() {
     }
 
     header.addEventListener('click', () => {
+      document.querySelectorAll('.dropdown.dropdown--expanded').forEach(opened => {
+        if (opened !== dropdown) opened.classList.remove('dropdown--expanded');
+      });
       dropdown.classList.toggle('dropdown--expanded');
       if (dropdown.classList.contains('dropdown--expanded')) {
         updateMinusButtons();
       }
     });
 
+    function getDeclensionsRooms(i, value) {
+      const declensions = [
+        [' спальня', ' спальни', ' спален'],
+        [' кровать', ' кровати', ' кроватей'],
+        [' ванная комната', ' ванные комнаты', ' ванных комнат']
+      ];
+      
+      if (value === 1) {
+        return declensions[i][0];
+      } else if (value > 1 && value < 5) {
+        return declensions[i][1];
+      } else {
+        return declensions[i][2];
+      }
+    }
+
+    function changeTitle() {
+      let result = [];
+      const counters = Array.from(dropdown.querySelectorAll('.dropdown__item-value'));
+
+      counters.forEach((counter, index) => {
+        const value = parseInt(counter.textContent, 10);
+        if (value > 0) {
+          result.push(value.toString() + getDeclensionsRooms(index, value));
+        }
+      });
+
+      if (result.length > 0) {
+        title.textContent = result.join(', ');
+      } else {
+        title.textContent = initialTitle;
+      }
+    }
+
     function increaseValue(button) {
       const elementCounter = button.previousElementSibling;
       elementCounter.textContent = (parseInt(elementCounter.textContent, 10) + 1).toString();
       updateMinusButtons();
-      footer.classList.add('dropdown__footer--open');
+      if (footer) {
+        footer.classList.add('dropdown__footer--open');
+      } else {
+        changeTitle();
+      }
     }
 
     function decreaseValue(button) {
@@ -39,6 +80,9 @@ function initDropdown() {
       if (valueCounter > 0) {
         elementCounter.textContent = (valueCounter - 1).toString();
         updateMinusButtons();
+      }
+      if (!footer) {
+        changeTitle();
       }
     }
 
@@ -57,43 +101,45 @@ function initDropdown() {
           increaseValue(button);
         } else if (button.classList.contains('dropdown__button--minus')) {
           decreaseValue(button);
-        } else if (button.classList.contains('dropdown__footer--clear')) {
-          clearValue();
         }
       })
     });
 
-    function getDeclensions(sum, type) {
-      const declensions = {
-        guests: ['гость', 'гостя', 'гостей'],
-        rooms: ['комната', 'комнаты', 'комнат']
-      };
+    if (footer != null) {
 
-      if (sum === 1) {
-        return declensions[type][0];
-      } else if (sum > 1 && sum < 5) {
-        return declensions[type][1];
-      } else {
-        return declensions[type][2];
-      }
-    }
+      clearButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        clearValue();
+      })
 
-    applyButton.addEventListener('click', (event) => {
-      event.stopPropagation();
-      const counters = Array.from(dropdown.querySelectorAll('.dropdown__item-value'));
-      const sum = counters.reduce((currentSum, currentsum) => {
-        return currentSum + parseInt(currentsum.textContent, 10);
-      }, 0);
-      const dropdownType = dropdown.dataset.dropdownType;
+      function getDeclensionsGuests(sum) {
+        const declensions = ['гость', 'гостя', 'гостей'];
 
-      if (sum === 0) {
-        title.textContent = initialTitle;
-      } else {
-        title.textContent = `${sum} ${getDeclensions(sum, dropdownType)}`;
+        if (sum === 1) {
+          return declensions[0];
+        } else if (sum > 1 && sum < 5) {
+          return declensions[1];
+        } else {
+          return declensions[2];
+        }
       }
 
-      clearButton.classList.add('visible');
-    });
+      applyButton.addEventListener('click', (event) => {
+        event.stopPropagation();
+        const counters = Array.from(dropdown.querySelectorAll('.dropdown__item-value'));
+        const sum = counters.reduce((currentSum, counter) => {
+          return currentSum + parseInt(counter.textContent, 10);
+        }, 0);
+
+        if (sum === 0) {
+          title.textContent = initialTitle;
+        } else {
+          title.textContent = `${sum} ${getDeclensionsGuests(sum)}`;
+        }
+
+        clearButton.classList.add('visible');
+      });
+     }
   })
 };
 
